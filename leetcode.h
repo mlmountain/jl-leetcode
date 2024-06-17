2f8bd7194ef6160811db149d97bdcef8617452c5
mlmountain <134593807+mlmountain@users.noreply.github.com>
2024-06-17 17:14:11 +0800

    Create leetcode.h

---
+++ leetcode.h
@@ -0,0 +1,178 @@
+#ifndef LEETCODE_H
+#define LEETCODE_H
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    /*
+     * leetcode 88. 合并两个有序数组
+     * 给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+     * 请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+     * 注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
+     * 示例1:
+     * 输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+     * 输出：[1,2,2,3,5,6]
+     * 解释：需要合并 [1,2,3] 和 [2,5,6] 。
+     * 合并结果是 [1,2,2,3,5,6] ，其中斜体加粗标注的为 nums1 中的元素。
+     */
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n);
+    void testMerge();
+
+    /*
+     * leetcode 27. 移除元素
+     * 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+     * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+     * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+     * 示例1:
+     * 输入：nums = [3,2,2,3], val = 3
+     * 输出：2, nums = [2,2]
+     * 解释：函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。你不需要考虑数组中超出新长度后面的元素。例如，函数返回的新长度为 2 ，而 nums = [2,2,3,3] 或 nums = [2,2,0,0]，也会被视作正确答案。
+     */
+    int removeElement(vector<int>& nums, int val);
+    void testRemoveElement();
+
+    /*
+     * leetcode 26. 删除有序数组中的重复项
+     * 给你一个 非严格递增排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。然后返回 nums 中唯一元素的个数。
+     * 考虑 nums 的唯一元素的数量为 k ，你需要做以下事情确保你的题解可以被通过：
+     * (1) 更改数组 nums ，使 nums 的前 k 个元素包含唯一元素，并按照它们最初在 nums 中出现的顺序排列。nums 的其余元素与 nums 的大小不重要。
+     * (2) 返回 k 。
+     * 示例 1：
+     * 输入：nums = [1,1,2]
+     * 输出：2, nums = [1,2,_]
+     * 解释：函数应该返回新的长度 2 ，并且原数组 nums 的前两个元素被修改为 1, 2 。不需要考虑数组中超出新长度后面的元素。
+     */
+    int removeDuplicates(vector<int>& nums);
+    void testRemoveDuplicates();
+
+    /*
+     * leetcode 80. 删除有序数组中的重复项 II
+     * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使得出现次数超过两次的元素只出现两次 ，返回删除后数组的新长度。
+     * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+     * 示例 1：
+     * 输入：nums = [1,1,1,2,2,3]
+     * 输出：5, nums = [1,1,2,2,3]
+     * 解释：函数应返回新长度 length = 5, 并且原数组的前五个元素被修改为 1, 1, 2, 2, 3。 不需要考虑数组中超出新长度后面的元素。
+     */
+    int removeDuplicates2(vector<int>& nums);
+    void testremoveDuplicates2();
+
+    /*
+     * leetcode 42. 接雨水
+     * 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+     * :/image/leetcode_42.png
+     * 示例 1：
+     * 输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+     * 输出：6
+     * 解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。
+     */
+    int trap(vector<int>& height);
+    void testTrap();
+
+    /*
+     * leetcode 407. 接雨水 II
+     * 给你一个 m x n 的矩阵，其中的值均为非负整数，代表二维高度图每个单元的高度，请计算图中形状最多能接多少体积的雨水。
+     * :/image/leetcode_407.png
+     * 输入: heightMap = [[1,4,3,1,3,2],[3,2,1,3,2,4],[2,3,3,2,3,1]]
+     * 输出: 4
+     * 解释: 下雨后，雨水将会被上图蓝色的方块中。总的接雨水量为1+2+1=4。
+     */
+    int trapRainWater(vector<vector<int>>& heightMap);
+    void testRrapRainWater();
+
+
+
+    /* 动态规划(Dynamic Programming)
+     * 背包问题: dp[i][j] 表示从下标为[0-i]的物品里任意取，放进容量为j的背包，价值总和最大是多少。
+     * 背包问题: 每一件物品其实只有两个状态，取或者不取
+     *
+     * 动态规划五部曲
+     * 1. 确定dp数组（dp table）以及下标的含义
+     * 2. 确定递推公式
+     * 3. dp数组如何初始化
+     * 4. 确定遍历顺序
+     * 5. 举例推导dp数组
+     */
+    /*
+     * leetcode 509
+     * 斐波那契数 （通常用 F(n) 表示）形成的序列称为 斐波那契数列 。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
+     * F(0) = 0，F(1) = 1
+     * F(n) = F(n - 1) + F(n - 2)，其中 n > 1
+     * 给定 n ，请计算 F(n) 。
+     */
+    int fib(int n);
+    void testFib();
+
+    /*
+     * leetcode 70 爬楼梯
+     * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+     * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+     */
+    int climbStairs(int n);
+    void testClimbStairs();
+
+    /*
+     * leetcode 746 使用最小花费爬楼梯
+     * 给你一个整数数组 cost ，其中 cost[i] 是从楼梯第 i 个台阶向上爬需要支付的费用。一旦你支付此费用，即可选择向上爬一个或者两个台阶。
+     * 你可以选择从下标为 0 或下标为 1 的台阶开始爬楼梯。
+     * 请你计算并返回达到楼梯顶部的最低花费。
+     */
+    int minCostClimbingStairs(vector<int>& cost);
+    void testMinCostClimbingStairs();
+
+    /*
+     * leetcode 1137 第 N 个泰波那契数
+     * 泰波那契序列 Tn 定义如下：
+     * T0 = 0, T1 = 1, T2 = 1, 且在 n >= 0 的条件下 Tn+3 = Tn + Tn+1 + Tn+2
+     * 给你整数 n，请返回第 n 个泰波那契数 Tn 的值。
+     */
+    int tribonacci(int n);
+    void testTribonacci();
+
+    /*
+     * leetcode 2244 完成所有任务需要的最少轮数
+     * 给你一个下标从 0 开始的整数数组 tasks ，其中 tasks[i] 表示任务的难度级别。在每一轮中，你可以完成 2 个或者 3 个 相同难度级别 的任务。
+     * 返回完成所有任务需要的 最少 轮数，如果无法完成所有任务，返回 -1 。
+     */
+    int minimumRounds(vector<int>& tasks);
+    void testMinimumRounds();
+
+    /*
+     * leetcode 62. 不同路径
+     * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+     * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+     * 问总共有多少条不同的路径？
+     */
+    int uniquePaths(int m, int n);
+    void testUniquePaths();
+
+    /*
+     * leetcode 63. 不同路径 II
+     * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+     * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish”）。
+     * 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+     * 网格中的障碍物和空位置分别用 1 和 0 来表示。
+     */
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid);
+    void testUniquePathsWithObstacles();
+
+    /*
+     * leetcode 64. 最小路径和
+     * 给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+     * 说明：每次只能向下或者向右移动一步。
+     */
+    int minPathSum(vector<vector<int>>& grid);
+    void testMinPathSum();
+
+    /*
+     * leetcode 343. 整数拆分
+     * 给定一个正整数 n ，将其拆分为 k 个 正整数 的和（ k >= 2 ），并使这些整数的乘积最大化。
+     * 返回 你可以获得的最大乘积 。
+     */
+    int integerBreak(int n);
+    void testIntegerBreak();
+
+};
+
+#endif // LEETCODE_H
