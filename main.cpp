7ad5836de4d11f4b4aa239fbefabd614d4b84684
mlmountain <134593807+mlmountain@users.noreply.github.com>
2024-06-18 09:45:03 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "leetcode.h"
+
+int main()
+{
+    Solution s;
+    // s.testMerge();
+    // s.testRemoveElement();
+    // s.testRemoveDuplicates();
+    // s.testTrap();
+    // s.testRrapRainWater();
+    // s.testremoveDuplicates2();
+    // s.testMinimumRounds();
+    // s.testUniquePaths();
+    // s.testUniquePathsWithObstacles();
+    // s.testMinPathSum();
+    // s.testIntegerBreak();
+    s.testNumTrees();
+
+    return 0;
+}
